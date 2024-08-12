62fe0f084831a8259cac19cbb70df90eb191e423
Allien10X <erdemiryz38@gmail.com>
2024-08-12 14:20:13 -0700

    a34x: Set ueventd.rc in recovery
    Signed-off-by: Allien10X <erdemiryz38@gmail.com>

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -15,3 +15,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := root/init.recovery.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.rc-recovery
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := root/ueventd.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
