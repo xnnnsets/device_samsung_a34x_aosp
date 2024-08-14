a7c6eea56253e052c5fcc4bebd62b63af6bc211e
Allien10X <erdemiryz38@gmail.com>
2024-08-14 07:04:46 -0700

    a34x: Configure Display stack
    Signed-off-by: Allien10X <erdemiryz38@gmail.com>
    
    * Reference: https://github.com/moto-common/android_device_mediatek_common/blob/master/vendor/display/mt6768/proprietary-files.txt

--- device.mk
+++ device.mk
@@ -41,6 +41,15 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.3-service \
+    android.hardware.memtrack-service.mediatek-mali \
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@4.0.vendor \
+    libdrm.vendor
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
