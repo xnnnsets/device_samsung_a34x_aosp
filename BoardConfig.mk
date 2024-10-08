1a28ebf658b86e0cf528002e5d4f474fe1046c82
MoonLight <sametsonerkarahasan@outlook.com>
2024-10-08 17:19:40 +0300

    Update BoardConfig.mk
    Fixed by removing irrelevant crap in Device Path variable

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/samsung/a34xp
+DEVICE_PATH := device/samsung/a34x
 
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
